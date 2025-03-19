b0641eea489b7d02931dbcf7ef34c13dce994cfc
Ni-aina <niaina.dev.soft@gmail.com>
2025-03-19 09:36:37 +0300

    Navbar responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      Home page
     </div>
   );
 }
