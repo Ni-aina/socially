52a42aecc97f4839f673fa82d4ce2f5e1b338a90
Ni-aina <niaina.dev.soft@gmail.com>
2025-03-20 20:16:47 +0300

    Profile page

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -58,7 +58,7 @@ export const getDbUserId = async ()=> {
 
     const user = await getUserByClerkId(clerkId);
 
-    if (!user) throw new Error("User not found");
+    if (!user) return;
 
     return user.id;
 }
